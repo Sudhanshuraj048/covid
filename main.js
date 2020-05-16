de69a427bd7c18dd19586d081bc54df283f9f2b9
sudhanshu ranjan <40245488+Sudhanshuraj048@users.noreply.github.com>
2020-05-16 21:04:13 +0530

    Update main.js

--- main.js
+++ main.js
@@ -113,6 +113,23 @@
       $('#header').addClass('header-scrolled');
     }
 
+    // Back to top button
+  // $(window).scroll(function() {
+  //   if ($(this).scrollTop() > 100) {
+  //     $('.back-to-top').fadeIn('slow');
+  //   } else {
+  //     $('.back-to-top').fadeOut('slow');
+  //   }
+  // });
+
+  $('.back-to-top').click(function() {
+    $('html, body').animate({
+      scrollTop: 0
+    }, 1500, 'easeInOutExpo');
+    return false;
+  });
+
+  
     // Initi AOS
     AOS.init({
       duration: 1000,
